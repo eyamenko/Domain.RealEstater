91bed6dfff6d01ca63f8b630b09db094468dc690
Eugene Yamenko <yamenko@me.com>
2017-12-12 23:24:28 +1100

    Add Health Check endpoint

---
+++ src/Domain.RealEstater.Web/Controllers/HealthCheckController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Domain.RealEstater.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class HealthCheckController : Controller
+    {
+        public IActionResult Check()
+        {
+            return Ok("I'm alive");
+        }
+    }
+}

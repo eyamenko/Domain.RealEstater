951e7e1e7543c7f1b856cdce08c95fda389ffacc
Eugene Yamenko <yamenko@me.com>
2017-12-12 19:00:24 +1100

    * Add Rider's settings folder to .gitignore * Add Contracts and Models projects * Add Property model * Add IPropertyMatcher interface

---
+++ src/Domain.RealEstater.Contracts/IPropertyMatcher.cs
@@ -0,0 +1,9 @@
+﻿using Domain.RealEstater.Models;
+
+namespace Domain.RealEstater.Contracts
+{
+    public interface IPropertyMatcher
+    {
+        bool IsMatch(Property agencyProperty, Property databaseProperty);
+    }
+}

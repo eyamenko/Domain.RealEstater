699d87b0509f915e804ade6b0b7cd14bbdbfeb77
Eugene Yamenko <yamenko@me.com>
2017-12-12 19:04:07 +1100

    Add stub matcher classes

---
+++ src/Domain.RealEstater.Matchers/ContraryRealEstatePropertyMatcher.cs
@@ -0,0 +1,13 @@
+﻿using Domain.RealEstater.Contracts;
+using Domain.RealEstater.Models;
+
+namespace Domain.RealEstater.Matchers
+{
+    public class ContraryRealEstatePropertyMatcher : IPropertyMatcher
+    {
+        public bool IsMatch(Property agencyProperty, Property databaseProperty)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/Domain.RealEstater.Matchers/OnlyTheBestRealEstatePropertyMatcher.cs
@@ -0,0 +1,13 @@
+﻿using Domain.RealEstater.Contracts;
+using Domain.RealEstater.Models;
+
+namespace Domain.RealEstater.Matchers
+{
+    public class OnlyTheBestRealEstatePropertyMatcher : IPropertyMatcher
+    {
+        public bool IsMatch(Property agencyProperty, Property databaseProperty)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

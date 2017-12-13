f7df52a6dc5ca80cac34c0a52b6adfcb93409fdb
Eugene Yamenko <yamenko@me.com>
2017-12-13 20:15:15 +1100

    * Add service project * Stub Property Worker's implementation

---
+++ src/Domain.RealEstater.Service/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Domain.RealEstater.Contracts.Data;
+using Domain.RealEstater.Contracts.Services;
+using Domain.RealEstater.Data;
+using Domain.RealEstater.Models;
+using Domain.RealEstater.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceNetCore.Contracts;
+
+namespace Domain.RealEstater.Service
+{
+    public class Startup : IStartup
+    {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            #region Services
+
+            services.AddSingleton<IQueueService<Property>, PropertyQueueService>();
+
+            #endregion
+
+            #region Data
+
+            services.AddSingleton<IConnectionFactory>(_ =>
+                new MySqlConnectionFactory(_configuration.GetConnectionString("RealEstater")));
+
+            services.AddSingleton<IQueueRepository, QueueRepository>();
+
+            #endregion
+        }
+    }
+}
---
+++ src/Domain.RealEstater.Service/Workers/PropertyWorker.cs
@@ -0,0 +1,43 @@
+﻿using Domain.RealEstater.Contracts.Services;
+using Domain.RealEstater.Models;
+using ServiceNetCore;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Domain.RealEstater.Service.Workers
+{
+    public class PropertyWorker : Worker
+    {
+        private const int OneSecond = 1000;
+
+        private readonly IQueueService<Property> _queueService;
+
+        private Timer _timer;
+
+        public PropertyWorker(IQueueService<Property> queueService)
+        {
+            _queueService = queueService;
+        }
+
+        public override void Start()
+        {
+            _timer = new Timer(_ => Callback().GetAwaiter().GetResult());
+            _timer.Change(OneSecond, OneSecond);
+        }
+
+        public override void Stop()
+        {
+            _timer.Dispose();
+        }
+
+        private async Task Callback()
+        {
+            var property = await _queueService.Peek();
+
+            if (property == null)
+            {
+                return;
+            }
+        }
+    }
+}

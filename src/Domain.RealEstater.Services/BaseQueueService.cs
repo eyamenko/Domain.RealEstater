7684d89ae771b9776cd046f6774d78e6fd79ce37
Eugene Yamenko <yamenko@me.com>
2017-12-13 19:31:16 +1100

    Make PropertyQueueService implement IQueueService interface

--- src/Domain.RealEstater.Services/BaseQueueService.cs
+++ src/Domain.RealEstater.Services/BaseQueueService.cs
@@ -1,17 +1,44 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using Domain.RealEstater.Contracts.Data;
+using Domain.RealEstater.Models;
 
 namespace Domain.RealEstater.Services
 {
-    public class BaseQueueService<T>
+    public abstract class BaseQueueService<T>
     {
+        private readonly IQueueRepository _queueRepository;
+
+        protected BaseQueueService(IQueueRepository queueRepository)
+        {
+            _queueRepository = queueRepository;
+        }
+
+        protected abstract string Queue { get; }
+
         public async Task Push(T message)
         {
-            
+            var queueItem = new QueueItem<T>
+            {
+                Id = Guid.NewGuid(),
+                Message = message
+            };
+
+            await _queueRepository.Push(Queue, queueItem);
         }
 
         public async Task<T> Peek()
         {
-            
+            var queueItem = await _queueRepository.Peek<T>(Queue);
+
+            if (queueItem == null)
+            {
+                return default(T);
+            }
+
+            await _queueRepository.MarkAsDequeued(Queue, queueItem.Id);
+
+            return queueItem.Message;
         }
     }
 }
